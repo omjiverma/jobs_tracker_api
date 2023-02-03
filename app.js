718e132394d05a6fe5b8582d488cc5635121be92
omjiverma <omjidvlpr@gmail.com>
2023-02-03 16:29:16 +0530

    not-found middleware

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 
+const notFoundMiddleware = require('./middlewares/notFound.middleware')
+
+// Middleware
+
+
 // Routes
 app.get('/', (req, res) => {
     res.send("Get Request to home routes")
 })
+
+// Middleware
+app.use(notFoundMiddleware)
 
 
 module.exports = app;
