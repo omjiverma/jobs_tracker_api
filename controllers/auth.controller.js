2ac7b20fec3599abda6d2184c8ca94633634b678
omjiverma <omjidvlpr@gmail.com>
2023-02-05 11:33:21 +0530

    passing error to error handler middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,11 @@
 const User = require('../models/user.model')
 
-const register = async(req, res) =>{
+const register = async(req, res, next) =>{
     try {
         const user = await User.create(req.body)
         res.status(201).json({user})
     } catch (error) {
-        res.status(500).json({msg: "something went wrong"})
+        next(error)
     }
 }
 
--- middlewares/errorHandler.middleware.js
+++ middlewares/errorHandler.middleware.js
@@ -1,6 +1,6 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: "Some thing went wrong" });
+  res.status(500).json({ msg: err });
 };
 
 module.exports = errorHandlerMiddleware
