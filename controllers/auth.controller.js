079a7329f4e8c804c98e33ca1bacd9ecd41fa6dc
omjiverma <omjidvlpr@gmail.com>
2023-02-06 10:27:28 +0530

    Duplicate email validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,16 @@
 const User = require("../models/user.model");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, NotFoundError } = require('../errors/custom.errors')
+const { BadRequestError, NotFoundError } = require("../errors/custom.errors");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all values");
+  }
+
+  const userAlreadyExists = await User.find({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email Already in use");
   }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
